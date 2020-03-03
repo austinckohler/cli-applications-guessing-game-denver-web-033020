33ac40f59b222deb058e6a8f988bcc1953898f44
Austin Kohler <austinckohler@gmail.com>
2020-03-03 17:22:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,10 @@
 
 
 def run_guessing_game
-  random = rand(6) + 1 # generates a random number 1 -6 
+  random = rand(6) + 1 # generates a random number 1 -6, (not a string)
   puts "Guess a number between 1 and 6" #asking the question 
   user_input = gets.chomp #Capture user input
-  if user_input == random.to_s #Compare that input to the random number that has been generated
+  if user_input == random.to_s #Compare that input to the random number that has been generated. use .to_s to convert integer to string 
     puts "You guessed the correct number!" #Prints if guessed random number correctly
   elsif user_input == "exit" 
     puts "Goodbye!"
